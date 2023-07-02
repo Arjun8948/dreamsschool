fbce163c954363e6582b14589a21e79f3c2a043d
arvind <arvindcomputer7@gmail.com>
2023-07-02 22:11:14 +0530

    ss

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 import express from "express";
-import dotenv from "dotenv";
-import DbConnection from "./connection/dbConnetion.js";
-import route from "./router/routes.js";
+// import dotenv from "dotenv";
+// import DbConnection from "./connection/dbConnetion.js";
+// import route from "./router/routes.js";
  
 
 const app = express();
 app.use(express.json());
  
-app.use("/api", route);
-dotenv.config();
-app.use("/upload", express.static("upload"));
+// app.use("/api", route);
+
+// dotenv.config();
+
+// app.use("/upload", express.static("upload"));
 
 // error massage middleware function
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const massage = err.massage || "Something wrong";
-  return res.status(status).json({ sucess: false, status, massage });
-});
+// app.use((err, req, res, next) => {
+//   const status = err.status || 500;
+//   const massage = err.massage || "Something wrong";
+//   return res.status(status).json({ sucess: false, status, massage });
+// });
 
 app.get("/", (req, res) => {
   res.send("working");
 });
 
-app.listen(process.env.port, () => {
-  DbConnection();
+app.listen(3000, () => {
+  // DbConnection();
   console.log(`Server is running ${process.env.port}`);
 });
 
